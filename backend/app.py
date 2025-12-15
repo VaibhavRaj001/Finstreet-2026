340c7e45f6d511a7e0da950a60f4b4e0ba7fd402
Ved Sharma <vedsharmabgt@gmail.com>
2025-12-16 00:53:01 +0530

    connected mysql and tested it

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.health import health_bp
+from routes.db_test import db_bp
+
 
 app = Flask(__name__)
 CORS(app)
-
+app.register_blueprint(db_bp, url_prefix="/api")
 # register blueprints
 app.register_blueprint(health_bp, url_prefix="/api")
 
